fd39b0fe8ca2b4466a669b67548ce65c7320bde0
Shayan <xenos_pr@yahoo.com>
2026-02-07 01:23:04 +0400

    Added contact us page to the models.py

--- home/models.py
+++ home/models.py
@@ -1,10 +1,41 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 from wagtail.models import Page
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.fields import StreamField
-from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField, RichTextField
+from wagtail.admin.panels import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        "ContactPage", on_delete=models.CASCADE, related_name="form_fields"
+    )
+
+
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("from_address", classname="col6"),
+                        FieldPanel("to_address", classname="col6"),
+                    ]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email Configuration",
+        ),
+    ]
 
 
 class SocialLinkBlock(blocks.StructBlock):
